93817af71f5e8eab17b77f34ec42dad307baca4c
callahn mcdonald <callahn@gmail.com>
2022-10-28 14:57:01 -0600

    getting outside help

---
+++ Routes/API/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const userRoutes = require("./user-route");
+const thoughtRoutes = require("./thought-route");
+
+router.use('/users', userRoutes);
+
+module.exports = router;
